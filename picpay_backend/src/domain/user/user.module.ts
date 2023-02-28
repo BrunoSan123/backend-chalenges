53a967fd7f2f16f34e235465bf5847d078385d6b
Bruno Barreto <Bruno-kk@hotmail.com>
2023-02-28 20:11:23 -0300

    fix: typeorm module added to user module

--- picpay_backend/src/domain/user/user.module.ts
+++ picpay_backend/src/domain/user/user.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './use_cases/user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entity/user.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([UserEntity])],
   providers: [UserService]
 })
 export class UserModule {}
